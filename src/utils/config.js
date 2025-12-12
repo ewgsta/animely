9151123f68d995c69428becc298e3568d666b1ea
ewgsta <159681870+ewgsta@users.noreply.github.com>
2025-12-12 20:30:51 +0300

    Çiftçi tona

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,10 @@
 // @ts-check
 import fs from "fs";
 import path from "path";
+import { execSync } from "child_process";
+import os from "os";
 
-const configPath = path.join(process.cwd(), "config.json");
+const configPath = path.join(process.cwd(), ".config.animely");
 
 const defaultConfig = {
 	maxConcurrent: 3,
@@ -37,4 +39,12 @@ export function getConfig() {
  */
 export function saveConfig(config) {
 	fs.writeFileSync(configPath, JSON.stringify(config, null, 2), "utf-8");
+	
+	if (os.platform() === "win32") {
+		try {
+			execSync(`attrib +h "${configPath}"`);
+		} catch (error) {
+			// Olmazsa siktir et amına koyayım.
+		}
+	}
 }
